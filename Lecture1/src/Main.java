ea8f00638dec425a74fc1e8a34a684788d959503
R2D2 <artur0y95@gmail.com>
2025-10-16 22:42:15 +0200

    poprawiono modyfikatory dostępu metody main

--- Lecture1/src/Main.java
+++ Lecture1/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 
 public class Main {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         // deklaracja zmiennych typu int
         int a, b;
 
